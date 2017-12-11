0992be76c9e8b74e6e54f9d9bd4e3a29a71553b1
César <Csar@DESKTOP-F5G83SO>
2017-12-11 19:43:05 +0100

    Oui

--- src/series/oui.java
+++
@@ -1,9 +0,0 @@
-package series;
-
-import java.io.File;
-
-public class oui {
-	public static void main(String args[]) {
-		System.out.println(new File("src\\resources\\series.csv").exists());
-	}
-}
