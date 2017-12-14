f089ff4d1530d50aa8bb6c0e6c89f4c4c7a0fb82
César <Csar@DESKTOP-F5G83SO>
2017-12-14 22:13:32 +0100

    Series list sorted

--- src/series/Model.java
+++ src/series/Model.java
@@ -1,6 +1,7 @@
 package series;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.Observable;
 
 public class Model extends Observable {
@@ -10,13 +11,14 @@ public class Model extends Observable {
 	private String tpsRestant = "0 jours 0 h 0 m";
 
 	public Serie getSerie(String s) {
-		for(Serie se : series) {
-			if(se.getNom().equals(s)) {
+		for (Serie se : series) {
+			if (se.getNom().equals(s)) {
 				return se;
 			}
 		}
 		return null;
 	}
+
 	public String getTpsRestant() {
 		return tpsRestant;
 	}
@@ -69,10 +71,18 @@ public class Model extends Observable {
 	}
 
 	public ArrayList<String> getNoms() {
+		class StringComparator implements Comparator<String> {
+			public int compare(String obj1, String obj2) {
+				return obj1.compareToIgnoreCase(obj2);
+			}
+		}
 		ArrayList<String> l = new ArrayList<String>();
 		for (Serie s : series) {
 			l.add(s.getNom());
 		}
+		l.sort(new StringComparator());
+		
+		
 		l.add("Total");
 		return l;
 	}
@@ -94,54 +104,54 @@ public class Model extends Observable {
 			}
 		}
 	}
-	
-	
+
 	private void selectTotal() {
-		 nbEpisodes=0;
-		 dureeAvg=0;
-		 nbVus=0;
-		 tpsRestant= "0 jours 0 h 0 m";
-		 int tpsTotal=0;
-		 for(Serie s : series) {
-			 nbEpisodes += s.getNbEpisodes();
-			 nbVus+= s.getNbVus();
-			 tpsTotal += s.getDureeRestanteInt();
-		 }
-		 if(nbEpisodes-nbVus != 0)
-			 dureeAvg = tpsTotal/(nbEpisodes-nbVus);
-		 Serie tps = new Serie("ptdr",1,tpsTotal,0);
-		 tpsRestant=tps.getDureeRestanteString();
-		 setChanged();
-		 notifyObservers();
-		
+		nbEpisodes = 0;
+		dureeAvg = 0;
+		nbVus = 0;
+		tpsRestant = "0 jours 0 h 0 m";
+		int tpsTotal = 0;
+		for (Serie s : series) {
+			nbEpisodes += s.getNbEpisodes();
+			nbVus += s.getNbVus();
+			tpsTotal += s.getDureeRestanteInt();
+		}
+		if (nbEpisodes - nbVus != 0)
+			dureeAvg = tpsTotal / (nbEpisodes - nbVus);
+		Serie tps = new Serie("ptdr", 1, tpsTotal, 0);
+		tpsRestant = tps.getDureeRestanteString();
+		setChanged();
+		notifyObservers();
+
 	}
 
 	public void add(String text, String text2, String text3, String text4) {
-		Serie tmp = new Serie(text, Integer.valueOf(text2), Integer.valueOf(text3),Integer.valueOf(text4));
+		Serie tmp = new Serie(text, Integer.valueOf(text2), Integer.valueOf(text3), Integer.valueOf(text4));
 		series.add(tmp);
 		setChanged();
 		notifyObservers("oui");
 	}
 
 	public void remove(String selectedItem) {
-		for(Serie s : series) {
-			if(s.getNom().equals(selectedItem)) {
+		for (Serie s : series) {
+			if (s.getNom().equals(selectedItem)) {
 				series.remove(s);
 				break;
 			}
 		}
 		setChanged();
 		notifyObservers("oui");
-		
+
 	}
 
 	public void edit(String selectedItem) {
 		// TODO Auto-generated method stub
-		
+
 	}
+
 	public void edit(String text, String text2, String text3, String text4) {
-		for(Serie s : series) {
-			if(s.getNom().equals(text)) {
+		for (Serie s : series) {
+			if (s.getNom().equals(text)) {
 				s.setNbEpisodes(Integer.valueOf(text2));
 				setNbEpisodes(s.getNbEpisodes());
 				s.setDureeAvg(Integer.valueOf(text3));
@@ -154,11 +164,12 @@ public class Model extends Observable {
 		}
 		setChanged();
 		notifyObservers();
-		
+
 	}
+
 	public void close() throws Exception {
 		Parser p = new Parser();
 		p.close(series);
-		
+
 	}
 }
